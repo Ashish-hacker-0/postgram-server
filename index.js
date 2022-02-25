d8f5e80c94851d58353cc658919bb955d5948cb0
Ashish-hacker-0 <ashish1222001@gmail.com>
2022-02-25 20:08:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
+console.log('env',process.env);
 const http = require('http');
 const express = require("express");
 const app = express();
